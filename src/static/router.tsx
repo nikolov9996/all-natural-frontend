feed3a348ac48e4e7546becb6ec6121822e0460f
Zhivko Nikolov <nikolov9996@gmail.com>
2023-12-07 22:08:30 +0200

    feat:router skeleton

---
+++ src/static/router.tsx
@@ -0,0 +1,10 @@
+import {
+    RouterProvider
+} from 'react-router-dom';
+import { router } from './routes.static';
+
+const AppRouter: React.FC = () => {
+    return <RouterProvider router={router} />
+}
+
+export default AppRouter
---
+++ src/static/routes.static.tsx
@@ -0,0 +1,11 @@
+import {
+  createBrowserRouter,
+} from 'react-router-dom';
+import Home from '~/components/Home';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+]);
