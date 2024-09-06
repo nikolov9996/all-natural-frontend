da061ece61b9e1a546fe8b9c6ce09065366c14c7
Zhivko Nikolov <nikolov9996@gmail.com>
2024-09-06 18:13:02 +0300

    feat: redux-persist for auth

--- src/services/authSliceAPI.ts
+++ src/services/authSliceAPI.ts
@@ -24,6 +24,7 @@ export interface LoginRequest {
 }
 
 export const authSliceAPI = createApi({
+  reducerPath:"auth-api",
   baseQuery: fetchBaseQuery({
     baseUrl: apiUrl,
     prepareHeaders: (headers, { getState }) => {
